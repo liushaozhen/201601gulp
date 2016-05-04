6f00829e5c9ef2b4ebd6be9271a969aef0fa869f
guoyongfeng <296512521@qq.com>
2016-05-04 16:36:17 +0800

    合并CSS

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,16 @@ var minifyCss = require('gulp-minify-css');
 
 gulp.task('concat',function(){
    gulp.src('app/css/*.css')
-       .pipe(concat('all.css'))//先合并
+       .pipe(concat('app.css'))//先合并
        .pipe(gulp.dest('dist/css'))//再保存合并之后压缩之前的文件
        .pipe(minifyCss())//压缩
-       .pipe(rename('all.min.css'))//重命名
+       .pipe(rename(function (path) {
+          //目录名
+          //path.dirname += "/ciao";
+          //文件名
+          path.basename += ".min";
+          //扩展名
+          ///path.extname = ".md"
+       }))//重命名
        .pipe(gulp.dest('dist/css'))//输出到目的地
 });
